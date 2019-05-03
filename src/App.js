f5d3d643e2f5ee60010e33835bda282ae0297517
shahnkha <blooshaanster@gmail.com>
2019-05-04 03:08:53 +0530

    clean up state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React, { Component } from "react";
 import WeatherSearch from "components/WeatherElements/WeatherSearch";
-import { getWeather } from "API";
+import { getWeather, getLocation } from "API";
+import { Row, Col, Card, CardBody } from "reactstrap";
 
 class App extends Component {
   state = {
-    //init marker coords
-    weather: { latitude: 12.9791198, longitude: 77.5912997 },
+    weather: null,
     //init map coords
     viewport: {
       latitude: 12.9791198,
       longitude: 77.5912997,
       zoom: 11
     },
+    //store gps location if available
+    proximity: {
+      latitude: null,
+      longitude: null
+    },
+    //current place
+    place_name: "",
+    //ui loaders
     isLoaded: false,
     isLoading: false
   };
@@ -19,11 +27,11 @@ class App extends Component {
   toggleLoading = () => this.setState({ isLoading: !this.state.isLoading });
 
   setViewport = viewport => {
-    this.setState({ ...this.state, viewport: viewport });
+    this.setState({ viewport: viewport });
     // console.log('set viewport in app js', this.state);
   };
 
-  performSearch = async (lat, lon) => {
+  performSearch = async (lat, lon, place) => {
     const res = await getWeather(lat, lon);
     // console.log("res data appjs", res);
     if (res !== 0) {
@@ -32,26 +40,68 @@ class App extends Component {
         viewport: {
           ...this.state.viewport,
           latitude: res.latitude,
-          longitude: res.longitude,
-          zoom: 11
+          longitude: res.longitude
         },
+        place_name: place,
         isLoaded: true,
         isLoading: false
       });
     }
   };
 
+  componentDidMount() {
+    // const { isLoaded, toggleLoading, performSearch } = this.props;
+    if (!this.state.isLoaded) {
+      if ("geolocation" in navigator) {
+        /* geolocation is available */
+        navigator.geolocation.getCurrentPosition(async position => {
+          const { latitude, longitude } = position.coords;
+          this.toggleLoading();
+          const res = await getLocation(latitude, longitude);
+          // console.log("navigator position", position);
+          this.setState({
+            proximity: { latitude: latitude, longitude: longitude }
+          });
+          this.performSearch(
+            latitude,
+            longitude,
+            res.features[0].place_name_en
+          );
+        });
+      } else {
+        /* geolocation IS NOT available */
+      }
+    }
+  }
+
   render() {
-    // console.log("App State", this.state);
+    console.log("App State", this.state);
     return (
       <div>
-        <WeatherSearch
-          weatherState={this.state}
-          performSearch={this.performSearch}
-          toggleLoading={this.toggleLoading}
-          setViewport={this.setViewport}
-          isLoaded={this.state.isLoaded}
-        />
+        {this.state.isLoaded ? (
+          <WeatherSearch
+            // weatherState={this.state}
+            // performSearch={this.performSearch}
+            // toggleLoading={this.toggleLoading}
+            // setViewport={this.setViewport}
+            currently={this.state.weather.currently}
+            isLoaded={this.state.isLoaded}
+            place_name={this.state.place_name}
+            proximity={this.state.proximity}
+          />
+        ) : (
+          <div className="content">
+            <Row className="text-center">
+              <Col xs={12} sm={6} md={6} lg={4}>
+                <Card className="card-stats ">
+                  <CardBody>
+                    <h3>Loading...</h3>
+                  </CardBody>
+                </Card>
+              </Col>
+            </Row>
+          </div>
+        )}
       </div>
     );
   }
--- src/components/WeatherElements/WeatherSearch.js
+++ src/components/WeatherElements/WeatherSearch.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 // import { AutoComplete } from "primereact/autocomplete";
-import { getLocation, getSuggestions } from "API";
+// import { getLocation, getSuggestions } from "API";
 import {
   Card,
   // CardHeader,
@@ -16,54 +16,19 @@ import Stats from "components/Stats/Stats.jsx";
 
 export default class WeatherSearch extends Component {
   state = {
-    //hold gps location for proximity suggestions
-    latitude: null,
-    longitude: null,
     //user entered text
     place: "",
     //place suggestions and coords in features
     suggestions: null,
-    features: null,
+    features: null
     //user selected text
-    place_name: ""
+    // place_name: ""
   };
-
-  componentDidMount() {
-    const { isLoaded, toggleLoading, performSearch } = this.props;
-    if (!isLoaded) {
-      if ("geolocation" in navigator) {
-        /* geolocation is available */
-        // const asyncGetCurrentPosition = options =>
-        //   new Promise((resolve, reject) => {
-        //     navigator.geolocation.getCurrentPosition(resolve, reject, options);
-        //   }); loc= await asyncGetCurrentPosition;
-        navigator.geolocation.getCurrentPosition(async position => {
-          const { latitude, longitude } = position.coords;
-          this.setState({
-            latitude: latitude,
-            longitude: longitude
-          });
-          toggleLoading();
-          const res = await getLocation(latitude, longitude);
-          // console.log("navigator position", position);
-          if (res !== 0) {
-            this.setState({
-              ...this.state,
-              place_name: res.features[0].place_name_en
-            });
-          }
-          performSearch(latitude, longitude);
-        });
-      } else {
-        /* geolocation IS NOT available */
-      }
-    }
-  }
 
   // loadSuggestions = async e => {
   //   const res = await getSuggestions(
-  //     this.state.latitude,
-  //     this.state.longitude,
+  //     this.props.proximity.latitude,
+  //     this.props.proximity.longitude,
   //     this.state.place
   //   );
   //   res !== 0
@@ -73,26 +38,25 @@ export default class WeatherSearch extends Component {
   //       })
   //     : // console.log('place state', this.state);
   //       this.setState({
-  //         ...this.state,
   //         features: null,
   //         suggestions: ["Place Not Found"]
   //       });
   // };
 
   onSelect = e => {
-    if (this.state.features !== null && this.state.features !== undefined) {
+    if (this.state.features !== null) {
       const { toggleLoading, performSearch } = this.props;
       const feature = this.state.features.filter(
         feature => feature.place_name === e.value
       );
       if (feature.length !== 0) {
-        // console.log('selected co-ords',feature[0].geometry.lat, latitude:feature[0].geometry.lat, this.state);
         toggleLoading();
         performSearch(
           feature[0].geometry.coordinates[1],
-          feature[0].geometry.coordinates[0]
+          feature[0].geometry.coordinates[0],
+          feature.place_name
         );
-        this.setState({ place: "", place_name: e.value });
+        // this.setState({ place: "", place_name: e.value });
       } else this.setState({ place: "" });
     } else this.setState({ place: "" });
   };
@@ -103,67 +67,60 @@ export default class WeatherSearch extends Component {
       size: 50,
       animate: true
     };
-    let temperature, icon, summary;
-    const { isLoaded } = this.props.weatherState;
-    if (isLoaded)
-      ({
-        temperature,
-        icon,
-        summary
-      } = this.props.weatherState.weather.currently);
+    const { temperature, icon, summary } = this.props.currently;
+    const { place_name } = this.props;
+    // const { isLoaded } = this.props.weatherState;
+    // if (isLoaded)
+    //   ({
+    //     temperature,
+    //     icon,
+    //     summary
+    //   } = this.props);
 
     return (
       <div className="content">
-        {isLoaded ? (
-          <Row>
-            <Col xs={12} sm={6} md={6} lg={3}>
-              <Card className="card-stats ">
-                <CardBody>
-                  <Row className="align-items-center">
-                    <Col xs={5} md={4}>
-                      <div className="text-center">
-                        <ReactAnimatedWeather
-                          icon={icon.replace(/-/g, "_").toUpperCase()}
-                          color={defaults.color}
-                          size={defaults.size}
-                          animate={defaults.animate}
-                        />
-                      </div>
-                    </Col>
-                    <Col xs={3} md={4}>
-                      <div>
-                        <p className="h5 text-muted text-center">{summary}</p>
-                      </div>
-                    </Col>
-                    <Col xs={7} md={4}>
-                      <div className="numbers text-center">
-                        <p className="card-category">Currently</p>
-                        <CardTitle tag="p">
-                          {Math.round(temperature)}°C
-                        </CardTitle>
-                      </div>
-                    </Col>
-                  </Row>
-                </CardBody>
-                <CardFooter>
-                  <hr />
-                  <Stats>
-                    {[
-                      {
-                        i: "fas fa-location-arrow",
-                        t: this.state.place_name
-                      }
-                    ]}
-                  </Stats>
-                </CardFooter>
-              </Card>
-            </Col>
-          </Row>
-        ) : (
-          <div className="lead">
-            <h3>Loading...</h3>
-          </div>
-        )}
+        <Row>
+          <Col xs={12} sm={6} md={6} lg={4}>
+            <Card className="card-stats ">
+              <CardBody>
+                <Row className="align-items-center justify-items-center">
+                  <Col xs={12} md={4}>
+                    <div className="text-center">
+                      <ReactAnimatedWeather
+                        icon={icon.replace(/-/g, "_").toUpperCase()}
+                        color={defaults.color}
+                        size={defaults.size}
+                        animate={defaults.animate}
+                      />
+                    </div>
+                  </Col>
+                  <Col xs={12} md={4}>
+                    <div>
+                      <p className="lead text-muted text-center">{summary}</p>
+                    </div>
+                  </Col>
+                  <Col xs={12} md={4}>
+                    <div className="numbers text-center">
+                      <p className="card-category">Currently</p>
+                      <CardTitle tag="p">{Math.round(temperature)}°C</CardTitle>
+                    </div>
+                  </Col>
+                </Row>
+              </CardBody>
+              <CardFooter>
+                <hr />
+                <Stats>
+                  {[
+                    {
+                      i: "fas fa-location-arrow",
+                      t: place_name
+                    }
+                  ]}
+                </Stats>
+              </CardFooter>
+            </Card>
+          </Col>
+        </Row>
         {/* <AutoComplete
           inputStyle={{ marginTop: "2px", width: "300px" }}
           placeholder="Enter Place Name"
@@ -180,8 +137,8 @@ export default class WeatherSearch extends Component {
 }
 
 WeatherSearch.propTypes = {
-  performSearch: PropTypes.func.isRequired,
-  toggleLoading: PropTypes.func.isRequired,
-  isLoaded: PropTypes.bool.isRequired,
-  weatherState: PropTypes.object.isRequired
+  // performSearch: PropTypes.func.isRequired,
+  // toggleLoading: PropTypes.func.isRequired,
+  isLoaded: PropTypes.bool.isRequired
+  // weatherState: PropTypes.object.isRequired
 };
