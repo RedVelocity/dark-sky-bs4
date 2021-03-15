b8e894301c32e3333800735ff11258d35f6cf0d8
Shah Nawaz Khan <blooshaanster@gmail.com>
2021-03-15 20:52:26 +0530

    res.json

--- functions/getWeather.js
+++ functions/getWeather.js
@@ -11,11 +11,13 @@ exports.handler = async (event, context) => {
   const params = querystring.parse(event.body);
   const { latitude, longitude } = params;
   const API_ENDPOINT = `https://api.darksky.net/forecast/${process.env.REACT_APP_DARKSKY_KEY}/${latitude},${longitude}?units=ca&exclude=[minutely,flags]`;
-  return fetch(API_ENDPOINT, { headers: { Accept: 'application/json' } })
-    .then((response) => response.json())
-    .then((data) => ({
-      statusCode: 200,
-      body: data,
-    }))
-    .catch((error) => ({ statusCode: 422, body: String(error) }));
+  return (
+    fetch(API_ENDPOINT, { headers: { Accept: 'application/json' } })
+      // .then((response) => response.json())
+      .then((data) => ({
+        statusCode: 200,
+        body: data,
+      }))
+      .catch((error) => ({ statusCode: 422, body: String(error) }))
+  );
 };
