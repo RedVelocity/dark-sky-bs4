e171f5ca7c812fa65ab5e095d03891c6d94e7ecc
Shah Nawaz Khan <blooshaanster@gmail.com>
2021-03-15 20:38:21 +0530

    add cors

--- functions/getWeather.js
+++ functions/getWeather.js
@@ -1,8 +1,13 @@
 const fetch = require('node-fetch');
 
 exports.handler = async (event, context) => {
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+  };
   if (event.httpMethod !== 'POST') {
-    return { statusCode: 405, body: 'Method Not Allowed' };
+    return { statusCode: 405, headers, body: 'Method Not Allowed' };
   }
   // When the method is POST, the name will no longer be in the event’s
   // queryStringParameters – it’ll be in the event body encoded as a query string
@@ -13,6 +18,7 @@ exports.handler = async (event, context) => {
     .then((response) => response.json())
     .then((data) => ({
       statusCode: 200,
+      headers,
       body: data,
     }))
     .catch((error) => ({ statusCode: 422, body: String(error) }));
