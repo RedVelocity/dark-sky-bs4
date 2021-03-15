19cd5fc4755d2909b6b74eb200d725021b1fdffd
Shah Nawaz Khan <blooshaanster@gmail.com>
2021-03-15 20:58:00 +0530

    axios

--- functions/getWeather.js
+++ functions/getWeather.js
@@ -1,4 +1,4 @@
-const fetch = require('node-fetch');
+const axios = require('axios');
 const querystring = require('querystring');
 
 exports.handler = async (event, context) => {
@@ -12,12 +12,16 @@ exports.handler = async (event, context) => {
   const { latitude, longitude } = params;
   const API_ENDPOINT = `https://api.darksky.net/forecast/${process.env.REACT_APP_DARKSKY_KEY}/${latitude},${longitude}?units=ca&exclude=[minutely,flags]`;
   return (
-    fetch(API_ENDPOINT, { headers: { Accept: 'application/json' } })
+    axios
+      .get(API_ENDPOINT, { headers: { Accept: 'application/json' } })
       // .then((response) => response.json())
-      .then((data) => ({
-        statusCode: 200,
-        body: data,
-      }))
+      .then((data) => {
+        console.log(data);
+        return {
+          statusCode: 200,
+          body: data,
+        };
+      })
       .catch((error) => ({ statusCode: 422, body: String(error) }))
   );
 };
